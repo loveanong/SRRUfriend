bac8414256d8d3f91314d2e49fd46278931bc526
anong <anong@mbw.ac.th>
2018-06-29 09:32:27 +0700

    chang size Image

--- app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/RegisterFragment.java
@@ -59,6 +59,9 @@ public class RegisterFragment extends Fragment {
             try {
 
                 Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,800,600,true);
+
                 imageView.setImageBitmap(bitmap);
 
             }catch (Exception e){
